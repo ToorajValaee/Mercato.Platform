a7f7f5a353040812a4dc1fa2ab8da2892b58e13b
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 01:34:28 +0330

    Add EF model builder extension foundation

---
+++ backend/src/Mercato.Infrastructure/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Mercato.Infrastructure.Data;
+
+public static class ModelBuilderExtensions
+{
+    public static void ConfigureMercatoModels(this ModelBuilder builder)
+    {
+        // Entity configurations will be registered here as modules grow.
+    }
+}
