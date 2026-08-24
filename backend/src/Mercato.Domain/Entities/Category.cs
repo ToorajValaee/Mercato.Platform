515ae45d5679b04fa9637d7e1aafde80a12dff7f
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 01:41:13 +0330

    Add hierarchical category support

--- backend/src/Mercato.Domain/Entities/Category.cs
+++ backend/src/Mercato.Domain/Entities/Category.cs
@@ -2,6 +2,7 @@ namespace Mercato.Domain.Entities;
 
 public class Category
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
+    public Guid? ParentId { get; set; }
 }
