482047d49bb44974002b494a7028249f86d889c5
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 01:37:02 +0330

    Add initial test foundation

---
+++ backend/tests/Mercato.Application.Tests/InventoryServiceTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Mercato.Application.Tests;
+
+public class InventoryServiceTests
+{
+    [Fact]
+    public void Foundation_Test_Should_Pass()
+    {
+        Assert.True(true);
+    }
+}
