50b174cd9ebb4a9afd1a57a8d797041af4bb4ba8
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 01:48:34 +0330

    Add Category entity configuration

---
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mercato.Domain.Entities;
+
+namespace Mercato.Infrastructure.Data.EntityConfigurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+    }
+}
