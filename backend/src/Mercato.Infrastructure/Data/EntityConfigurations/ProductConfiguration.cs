d61479c51f11db3a2eaa491e3a9f5794a3f673ff
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 01:49:05 +0330

    Complete Product EF Core entity configuration

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/ProductConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/ProductConfiguration.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mercato.Domain.Entities;
 
 namespace Mercato.Infrastructure.Data.EntityConfigurations;
 
-public class ProductConfiguration : IEntityTypeConfiguration<object>
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
 {
-    public void Configure(EntityTypeBuilder<object> builder)
+    public void Configure(EntityTypeBuilder<Product> builder)
     {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Sku).HasMaxLength(100);
+        builder.Property(x => x.Name).HasMaxLength(300).IsRequired();
+        builder.Property(x => x.Price).HasPrecision(18, 2);
     }
 }
