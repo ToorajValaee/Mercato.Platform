5bbdd088a61d81576a86d0ea92e2dbb5901de71c
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 21:43:26 +0330

    Add api smoke test foundation

---
+++ tests/Mercato.Api.Tests/ApiSmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Mercato.Api.Tests;
+
+public class ApiSmokeTests
+{
+    [Fact]
+    public void Api_Test_Project_Is_Restored()
+    {
+        Assert.True(true);
+    }
+}
