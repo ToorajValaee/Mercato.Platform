495c99b4898086ac51d021432bcccd7e457d58e1
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 21:43:16 +0330

    Add application checkout test foundation

---
+++ tests/Mercato.Application.Tests/CheckoutTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Mercato.Application.Tests;
+
+public class CheckoutTests
+{
+    [Fact]
+    public void Checkout_Test_Project_Is_Restored()
+    {
+        Assert.True(true);
+    }
+}
