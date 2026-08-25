6bedeaae6faff1c26fdf45bfdc298d3a714715df
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 22:59:57 +0330

    Add checkout workflow test coverage foundation

---
+++ tests/Mercato.Application.Tests/CheckoutWorkflowTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Mercato.Application.Tests;
+
+public class CheckoutWorkflowTests
+{
+    [Fact]
+    public void Checkout_flow_test_project_is_restored()
+    {
+        Assert.True(true);
+    }
+}
