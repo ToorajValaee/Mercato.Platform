55d9a67288aa53363f84cec0c4bac46d4dcb4acf
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-25 19:54:30 +0330

    Register inventory service implementation

--- backend/src/Mercato.Application/DependencyInjection.cs
+++ backend/src/Mercato.Application/DependencyInjection.cs
@@ -7,6 +7,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<Services.IProductService, Services.ProductServiceImplementation>();
+        services.AddScoped<Services.IInventoryService, Services.InventoryServiceImplementation>();
         services.AddScoped<Interfaces.IAuthService, Services.AuthService>();
         services.AddScoped<Services.PasswordService>();
 
