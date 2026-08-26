538c91430b1f8fe4b33fdf3f35a30a9c59971e34
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 00:57:11 +0330

    Expose branch selector as nopCommerce storefront widget

--- integrations/nopCommerce/Mercato.BranchSelector.Plugin/NopPlugin.cs
+++ integrations/nopCommerce/Mercato.BranchSelector.Plugin/NopPlugin.cs
@@ -1,7 +1,16 @@
+using Nop.Services.Cms;
 using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
 
 namespace Mercato.BranchSelector.Plugin;
 
-public sealed class MercatoBranchSelectorPlugin : BasePlugin
+public sealed class MercatoBranchSelectorPlugin : BasePlugin, IWidgetPlugin
 {
+    public bool HideInWidgetList => false;
+
+    public Task<IList<string>> GetWidgetZonesAsync()
+        => Task.FromResult<IList<string>>([PublicWidgetZones.HeaderSelectors]);
+
+    public Type GetWidgetViewComponent(string widgetZone)
+        => typeof(BranchSelectorViewComponent);
 }
