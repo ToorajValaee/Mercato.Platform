af65868006217b8a25a062e6b60e10d987fba4dc
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 01:10:23 +0330

    Remove obsolete settlement service stub

--- backend/src/Mercato.Application/Services/SettlementService.cs
+++
@@ -1,9 +0,0 @@
-namespace Mercato.Application.Services;
-
-public class SettlementService : ISettlementService
-{
-    public decimal CalculateArtistAmount(decimal purchasePrice, decimal soldQuantity)
-    {
-        return purchasePrice * soldQuantity;
-    }
-}
