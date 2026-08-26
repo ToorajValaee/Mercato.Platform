2e6bad571b9510fcaf26f5bba9e6e6553060150e
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-26 22:28:30 +0330

    Configure payment receipt reference

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/PaymentConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/PaymentConfiguration.cs
@@ -11,6 +11,8 @@ public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
         builder.HasKey(x => x.Id);
         builder.Property(x => x.Amount).HasPrecision(18, 2);
         builder.Property(x => x.Method).HasMaxLength(50).IsRequired();
+        builder.Property(x => x.Reference).HasMaxLength(80).IsRequired();
+        builder.HasIndex(x => x.Reference).IsUnique();
         builder.HasIndex(x => x.OrderId);
     }
 }
