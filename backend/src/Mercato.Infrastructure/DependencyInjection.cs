d002022699fc10b354f379e99401ae9e5ddf7a11
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-26 23:12:26 +0330

    Register inventory ledger repository

--- backend/src/Mercato.Infrastructure/DependencyInjection.cs
+++ backend/src/Mercato.Infrastructure/DependencyInjection.cs
@@ -20,6 +20,7 @@ public static class DependencyInjection
             options.UseNpgsql(connectionString));
 
         services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IInventoryRepository, InventoryRepository>();
         services.AddScoped<IOrderRepository, OrderRepository>();
         services.AddScoped<IInvoiceRepository, InvoiceRepository>();
         services.AddScoped<ISettlementRepository, SettlementRepository>();
