a47104461e877e3b8373406e8eefdbc063f4f8db
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 01:09:58 +0330

    Remove obsolete inventory service stub

--- backend/src/Mercato.Application/Services/InventoryService.cs
+++
@@ -1,9 +0,0 @@
-namespace Mercato.Application.Services;
-
-public class InventoryService : IInventoryService
-{
-    public Task<int> GetAvailableQuantityAsync(Guid productId, Guid branchId)
-    {
-        return Task.FromResult(0);
-    }
-}
