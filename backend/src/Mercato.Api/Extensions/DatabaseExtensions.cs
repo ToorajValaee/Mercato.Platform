d2d7c3c6aaa3ad1e4f6543120d3fd69cc75abd22
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 01:21:52 +0330

    Align API database initializer extension

--- backend/src/Mercato.Api/Extensions/DatabaseExtensions.cs
+++ backend/src/Mercato.Api/Extensions/DatabaseExtensions.cs
@@ -1,13 +1,14 @@
 using Mercato.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mercato.Api.Extensions;
 
 public static class DatabaseExtensions
 {
-    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    public static async Task InitializeDatabaseAsync(this WebApplication app, CancellationToken cancellationToken = default)
     {
         using var scope = app.Services.CreateScope();
-        var initializer = scope.ServiceProvider.GetRequiredService<DatabaseInitializer>();
-        await initializer.InitializeAsync();
+        var context = scope.ServiceProvider.GetRequiredService<MercatoDbContext>();
+        await DatabaseInitializer.InitializeAsync(context, cancellationToken);
     }
 }
