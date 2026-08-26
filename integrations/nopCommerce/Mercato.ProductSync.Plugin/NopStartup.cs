9d63b59f7d64743780e4eb9818b67f497c16bd50
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 00:32:56 +0330

    Register nopCommerce product sync services

---
+++ integrations/nopCommerce/Mercato.ProductSync.Plugin/NopStartup.cs
@@ -0,0 +1,30 @@
+using Mercato.NopCommerce.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Nop.Core.Infrastructure;
+
+namespace Mercato.ProductSync.Plugin;
+
+public sealed class NopStartup : INopStartup
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient("Mercato.ProductSync");
+        services.TryAddScoped(sp =>
+        {
+            var options = new MercatoConnectorOptions(
+                configuration["Mercato:BaseUrl"] ?? throw new InvalidOperationException("Mercato:BaseUrl is required."),
+                configuration["Mercato:BearerToken"] ?? string.Empty);
+            var factory = sp.GetRequiredService<IHttpClientFactory>();
+            return new MercatoApiClient(factory.CreateClient("Mercato.ProductSync"), options);
+        });
+        services.AddScoped<INopProductGateway, NopProductGateway>();
+        services.AddScoped<ProductSyncCore>();
+    }
+
+    public void Configure(IApplicationBuilder application) { }
+
+    public int Order => 3000;
+}
