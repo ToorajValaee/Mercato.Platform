65c1e22e9718b2ef31fc273de8b4e2644ff6d82b
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 01:21:37 +0330

    Align API service extension with infrastructure configuration

--- backend/src/Mercato.Api/Extensions/ServiceExtensions.cs
+++ backend/src/Mercato.Api/Extensions/ServiceExtensions.cs
@@ -1,13 +1,14 @@
-using Microsoft.Extensions.DependencyInjection;
 using Mercato.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Mercato.Api.Extensions;
 
 public static class ServiceExtensions
 {
-    public static IServiceCollection AddMercatoServices(this IServiceCollection services)
+    public static IServiceCollection AddMercatoServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddMercatoInfrastructure();
+        services.AddMercatoInfrastructure(configuration);
         return services;
     }
 }
