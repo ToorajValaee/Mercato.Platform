8c48152a5011d9eafe71c5574140e9977ef2a27f
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-26 22:25:19 +0330

    Implement payment entity configuration

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/PaymentConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/PaymentConfiguration.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mercato.Domain.Entities;
 
 namespace Mercato.Infrastructure.Data.EntityConfigurations;
 
-public class PaymentConfiguration : IEntityTypeConfiguration<object>
+public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
 {
-    public void Configure(EntityTypeBuilder<object> builder)
+    public void Configure(EntityTypeBuilder<Payment> builder)
     {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Amount).HasPrecision(18, 2);
+        builder.Property(x => x.Method).HasMaxLength(50).IsRequired();
+        builder.HasIndex(x => x.OrderId);
     }
 }
