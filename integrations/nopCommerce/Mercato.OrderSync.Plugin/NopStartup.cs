2da8082eb91985e207ca45bd1e6860a670ceddce
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 07:54:53 +0330

    Use connector-owned Mercato client in order sync plugin

--- integrations/nopCommerce/Mercato.OrderSync.Plugin/NopStartup.cs
+++ integrations/nopCommerce/Mercato.OrderSync.Plugin/NopStartup.cs
@@ -1,8 +1,6 @@
-using Mercato.NopCommerce.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Nop.Core.Infrastructure;
 
 namespace Mercato.OrderSync.Plugin;
@@ -11,19 +9,12 @@ public sealed class NopStartup : INopStartup
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddHttpClient("Mercato.OrderSync");
-        services.TryAddScoped(sp =>
-        {
-            var options = new MercatoConnectorOptions(
-                configuration["Mercato:BaseUrl"] ?? throw new InvalidOperationException("Mercato:BaseUrl is required."),
-                configuration["Mercato:BearerToken"] ?? string.Empty);
-            var factory = sp.GetRequiredService<IHttpClientFactory>();
-            return new MercatoApiClient(factory.CreateClient("Mercato.OrderSync"), options);
-        });
         services.AddScoped<OrderSyncCore>();
     }
 
-    public void Configure(IApplicationBuilder application) { }
+    public void Configure(IApplicationBuilder application)
+    {
+    }
 
-    public int Order => 3020;
+    public int Order => 2;
 }
