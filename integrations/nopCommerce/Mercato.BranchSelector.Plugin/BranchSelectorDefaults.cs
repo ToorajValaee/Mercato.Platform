480f361e1b77c25b373b9c95fe595c0b033dfc98
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 08:00:51 +0330

    Add branch selector route defaults

---
+++ integrations/nopCommerce/Mercato.BranchSelector.Plugin/BranchSelectorDefaults.cs
@@ -0,0 +1,7 @@
+namespace Mercato.BranchSelector.Plugin;
+
+public static class BranchSelectorDefaults
+{
+    public const string SelectBranchRouteName = "Mercato.BranchSelector.Select";
+    public const string SelectBranchRoutePattern = "mercato/branch/select";
+}
