9c837714f7623dd871c271ef21f97cdd2ee64e71
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 08:05:43 +0330

    Add nopCommerce configuration lifecycle to connector plugin

--- integrations/nopCommerce/Mercato.Connector.Plugin/NopPlugin.cs
+++ integrations/nopCommerce/Mercato.Connector.Plugin/NopPlugin.cs
@@ -1,8 +1,29 @@
+using Nop.Core;
 using Nop.Services.Common;
+using Nop.Services.Configuration;
 using Nop.Services.Plugins;
 
 namespace Mercato.Connector.Plugin;
 
 public sealed class MercatoConnectorPlugin : BasePlugin, IMiscPlugin
 {
+    private readonly IWebHelper _webHelper;
+    private readonly ISettingService _settings;
+
+    public MercatoConnectorPlugin(
+        IWebHelper webHelper,
+        ISettingService settings)
+    {
+        _webHelper = webHelper;
+        _settings = settings;
+    }
+
+    public override string GetConfigurationPageUrl()
+        => $"{_webHelper.GetStoreLocation()}Admin/MercatoConnector/Configure";
+
+    public override async Task UninstallAsync()
+    {
+        await _settings.DeleteSettingAsync<MercatoConnectorSettings>();
+        await base.UninstallAsync();
+    }
 }
