c6a261bf6bfa3b7d0748cc4825a9a7f5f806ff97
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-27 07:54:41 +0330

    Use connector-owned Mercato client in branch selector plugin

--- integrations/nopCommerce/Mercato.BranchSelector.Plugin/NopStartup.cs
+++ integrations/nopCommerce/Mercato.BranchSelector.Plugin/NopStartup.cs
@@ -1,8 +1,6 @@
-using Mercato.NopCommerce.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Nop.Core.Infrastructure;
 
 namespace Mercato.BranchSelector.Plugin;
@@ -11,20 +9,13 @@ public sealed class NopStartup : INopStartup
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddHttpClient("Mercato.BranchSelector");
-        services.TryAddScoped(sp =>
-        {
-            var options = new MercatoConnectorOptions(
-                configuration["Mercato:BaseUrl"] ?? throw new InvalidOperationException("Mercato:BaseUrl is required."),
-                configuration["Mercato:BearerToken"] ?? string.Empty);
-            var factory = sp.GetRequiredService<IHttpClientFactory>();
-            return new MercatoApiClient(factory.CreateClient("Mercato.BranchSelector"), options);
-        });
         services.AddScoped<BranchSelectorCore>();
         services.AddScoped<BranchSelectionService>();
     }
 
-    public void Configure(IApplicationBuilder application) { }
+    public void Configure(IApplicationBuilder application)
+    {
+    }
 
-    public int Order => 3030;
+    public int Order => 2;
 }
