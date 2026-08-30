390d6a28af4db36951fe8654e49a5f605904699a
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-30 11:43:57 +0330

    configure invoice subtotal and discount

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/InvoiceConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/InvoiceConfiguration.cs
@@ -9,6 +9,9 @@ public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
     public void Configure(EntityTypeBuilder<Invoice> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.SubtotalAmount).HasPrecision(18, 2);
+        builder.Property(x => x.DiscountAmount).HasPrecision(18, 2);
+        builder.Property(x => x.DiscountName).HasMaxLength(100);
         builder.Property(x => x.TotalAmount).HasPrecision(18, 2);
     }
 }
