8111e58b7c6ec09519841e6b46f76f268fe3e83a
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-30 11:47:07 +0330

    fix customer EF mapping and phone lookup

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/CustomerConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/CustomerConfiguration.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mercato.Domain.Entities;
 
 namespace Mercato.Infrastructure.Data.EntityConfigurations;
 
-public class CustomerConfiguration : IEntityTypeConfiguration<object>
+public sealed class CustomerConfiguration : IEntityTypeConfiguration<Customer>
 {
-    public void Configure(EntityTypeBuilder<object> builder)
+    public void Configure(EntityTypeBuilder<Customer> builder)
     {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Phone).HasMaxLength(50);
+        builder.Property(x => x.Email).HasMaxLength(320);
+        builder.HasIndex(x => x.Phone).IsUnique().HasFilter("\"Phone\" IS NOT NULL");
     }
 }
