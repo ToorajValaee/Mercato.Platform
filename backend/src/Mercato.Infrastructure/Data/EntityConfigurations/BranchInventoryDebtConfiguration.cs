cdf9be111cd2faae17b576dcf547d4edc1b7ef5e
Tooraj Valaee <tooraj.valaee@live.com>
2026-08-30 12:34:27 +0330

    map BranchInventoryDebt instead of System.Object

--- backend/src/Mercato.Infrastructure/Data/EntityConfigurations/BranchInventoryDebtConfiguration.cs
+++ backend/src/Mercato.Infrastructure/Data/EntityConfigurations/BranchInventoryDebtConfiguration.cs
@@ -1,11 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mercato.Domain.Entities;
 
 namespace Mercato.Infrastructure.Data.EntityConfigurations;
 
-public class BranchInventoryDebtConfiguration : IEntityTypeConfiguration<object>
+public sealed class BranchInventoryDebtConfiguration : IEntityTypeConfiguration<BranchInventoryDebt>
 {
-    public void Configure(EntityTypeBuilder<object> builder)
+    public void Configure(EntityTypeBuilder<BranchInventoryDebt> builder)
     {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Quantity).HasPrecision(18, 4);
+
+        builder.HasOne<Branch>()
+            .WithMany()
+            .HasForeignKey(x => x.FromBranchId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne<Branch>()
+            .WithMany()
+            .HasForeignKey(x => x.ToBranchId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne<Product>()
+            .WithMany()
+            .HasForeignKey(x => x.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
