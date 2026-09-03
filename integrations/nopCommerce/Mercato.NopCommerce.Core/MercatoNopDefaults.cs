90b0479a1622f478e73ed2f7af76de391cc9f2f8
Tooraj Valaee <tooraj.valaee@live.com>
2026-09-03 16:14:20 +0330

    add mercato category identity attribute

--- integrations/nopCommerce/Mercato.NopCommerce.Core/MercatoNopDefaults.cs
+++ integrations/nopCommerce/Mercato.NopCommerce.Core/MercatoNopDefaults.cs
@@ -3,6 +3,7 @@ namespace Mercato.NopCommerce.Core;
 public static class MercatoNopDefaults
 {
     public const string ProductIdAttribute = "Mercato.ProductId";
+    public const string CategoryIdAttribute = "Mercato.CategoryId";
     public const string BranchIdAttribute = "Mercato.BranchId";
     public const string CustomerIdAttribute = "Mercato.CustomerId";
     public const string OrderSyncedUtcAttribute = "Mercato.OrderSyncedUtc";
